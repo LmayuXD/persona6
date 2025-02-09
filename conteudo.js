9d71530291a3fcb24f87dfa2820dbf378a69aff4
Clebinhooo <lmayuyu11@gmail.com>
2025-02-09 16:39:59 -0300

    botão sair

--- conteudo.js
+++ conteudo.js
@@ -3,9 +3,10 @@ function loading() {
     document.getElementsByClassName("box-load")[0].style.display = "none";
     document.getElementsByClassName("conteudo")[0].style.display = "block";
 }
-// MENU.......
-//setup
 
 //CONTEÚDO
 
 //SAIR DO JOGO
+function sair(){
+    window.close();
+}
