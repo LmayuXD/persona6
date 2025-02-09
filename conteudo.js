9d1bb4e479f1d3a6b5d9efd30bfb367e0e4961ea
Clebinhooo <lmayuyu11@gmail.com>
2025-02-09 12:07:49 -0300

    deu certo :D

---
+++ conteudo.js
@@ -0,0 +1,5 @@
+//TELA DE CARREGAMENTO
+function loading() {
+    document.getElementsByClassName("box-load")[0].style.display = "none";
+    document.getElementsByClassName("conteudo")[0].style.display = "block";
+}
