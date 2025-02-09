5b57d19616a8e729e5e94756523e105f1d44f4d7
Clebinhooo <lmayuyu11@gmail.com>
2025-02-09 12:23:20 -0300

    potência

--- conteudo.js
+++ conteudo.js
@@ -3,3 +3,7 @@ function loading() {
     document.getElementsByClassName("box-load")[0].style.display = "none";
     document.getElementsByClassName("conteudo")[0].style.display = "block";
 }
+
+//CONTEÚDO
+
+//SAIR DO JOGO || MENU
