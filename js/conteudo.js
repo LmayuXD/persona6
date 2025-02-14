6eca0fec5673f2451f65062941def7ff2189715f
Clebinhooo <lmayuyu11@gmail.com>
2025-02-13 22:20:22 -0300

    tudo pra nada

--- js/conteudo.js
+++ js/conteudo.js
@@ -5,6 +5,38 @@ function loading() {
 }
 
 //CONTEÚDO
+document.getElementById("next-btn").style.cursor = "pointer";
+
+const historia = {
+    ato1: {
+        Text: "IS THAT THE GRIM REAPER??",
+        Image: "joker.jpeg",
+        next: []
+    },
+    ato2: {
+        Text: "Junpei? More like stupei!",
+        Image: "makoto.jpeg",
+        next: []
+    }
+};
+
+function carregarCena(cenaChave) {
+    const cena = historia[cenaChave];
+
+    document.getElementById("texto-da-historia").textContent = cena.Text;
+    document.getElementById("cena-do-personagem").src = cena.Image;
+
+    const proxCena = document.getElementById("next-btn");
+
+    cena.next.forEach(nexts => {
+        const button = document.createElement("button");
+        button.textContent = nexts.Text;
+        button.classList.add("next-button");
+        button.addEventListener("click", () => carregarCena(nexts));
+        proxCena.appendChild(button);
+    });
+    };
+carregarCena("ato1");
 
 //SAIR DO JOGO
 function sair() {
