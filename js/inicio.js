ae27e7498b219411be382824a42eb77415b039b9
Clebinhooo <lmayuyu11@gmail.com>
2025-02-22 13:35:25 -0300

    o botão de sair parou de funcionar(dnv)
    ta fd

--- js/inicio.js
+++ js/inicio.js
@@ -1,4 +1,16 @@
-//SAIR
+//ANIMAÇÃO DO FUNDO
+document.addEventListener("mousemove", (event) => {
+    const background = document.querySelector(".background");
+
+    //Captura a posição do mouse em relação ao centro da tela
+    let x = (event.clientX / window.innerWidth - 0.5)*20;
+    let y = (event.clientY / window.innerHeight - 0.5)*20;
+
+    //move a imagem levemente p/direção do cursor
+    background.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+//SAIR DO JOGO
 function sair() {
     window.close();
 }
