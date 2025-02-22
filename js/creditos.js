be031ca964aae670f0306aa9235c8170515091ba
Clebinhooo <lmayuyu11@gmail.com>
2025-02-22 12:41:33 -0300

    créditos

---
+++ js/creditos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const creditos = document.getElementById("creditos");
+
+    setTimeout(() => {
+        creditos.style.animation = "subir 20s ease-in-out forwards";
+    });
+});
